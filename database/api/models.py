439ae05c4b718b3eb1308b4394f816a2870ed63d
Timmy Yun <timur.unaspekov@nu.edu.kz>
2022-11-16 03:11:00 +0600

    Added MySQL connection and credentials.

--- database/api/models.py
+++ database/api/models.py
@@ -5,4 +5,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class DiseaseType(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True,
+                             serialize=True, verbose_name='ID', unique=True)
+    description = models.CharField(max_length=140)
+
 
--- database/database/settings.py
+++ database/database/settings.py
@@ -76,13 +76,17 @@ WSGI_APPLICATION = 'database.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dev',
+        'USER': 'timur',
+        'PASSWORD': '12345',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
